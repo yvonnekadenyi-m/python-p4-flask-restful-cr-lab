1815f2787321ff15095399ce1a93a8a209d814d4
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-21 14:34:26 +0300

    added to the files

--- server/app.py
+++ server/app.py
@@ -17,10 +17,28 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return [plant.to_dict() for plant in plants], 200
+    
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return new_plant.to_dict(), 201
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.get_or_404(id)
+        return plant.to_dict(), 200
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,3 +7,14 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price
+        }
